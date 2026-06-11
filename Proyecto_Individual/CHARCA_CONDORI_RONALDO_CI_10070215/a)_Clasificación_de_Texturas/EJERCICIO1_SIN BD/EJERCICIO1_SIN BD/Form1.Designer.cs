38ce383b478f5074d0298fc8789e3411398afe9e
CESAR <ronaldocharca3@gmail.com>
2026-06-10 20:25:35 -0400

    Add files via upload

---
+++ Proyecto_Individual/CHARCA_CONDORI_RONALDO_CI_10070215/a)_Clasificación_de_Texturas/EJERCICIO1_SIN BD/EJERCICIO1_SIN BD/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace EJERCICIO1_SIN_BD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureOriginal = new System.Windows.Forms.PictureBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnClasificar = new System.Windows.Forms.Button();
+            this.pictureResultado = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureOriginal
+            // 
+            this.pictureOriginal.Location = new System.Drawing.Point(164, 12);
+            this.pictureOriginal.Name = "pictureOriginal";
+            this.pictureOriginal.Size = new System.Drawing.Size(649, 571);
+            this.pictureOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureOriginal.TabIndex = 0;
+            this.pictureOriginal.TabStop = false;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(25, 171);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(100, 45);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar IMG";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnClasificar
+            // 
+            this.btnClasificar.Location = new System.Drawing.Point(25, 245);
+            this.btnClasificar.Name = "btnClasificar";
+            this.btnClasificar.Size = new System.Drawing.Size(100, 70);
+            this.btnClasificar.TabIndex = 2;
+            this.btnClasificar.Text = "CLASIFICAR TEXTURAS";
+            this.btnClasificar.UseVisualStyleBackColor = true;
+            this.btnClasificar.Click += new System.EventHandler(this.btnClasificar_Click);
+            // 
+            // pictureResultado
+            // 
+            this.pictureResultado.Location = new System.Drawing.Point(819, 12);
+            this.pictureResultado.Name = "pictureResultado";
+            this.pictureResultado.Size = new System.Drawing.Size(649, 571);
+            this.pictureResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureResultado.TabIndex = 5;
+            this.pictureResultado.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1496, 595);
+            this.Controls.Add(this.pictureResultado);
+            this.Controls.Add(this.btnClasificar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.pictureOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResultado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureOriginal;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnClasificar;
+        private System.Windows.Forms.PictureBox pictureResultado;
+    }
+}
+
---
+++ Proyecto_Individual/CHARCA_CONDORI_RONALDO_CI_10070215/a)_Clasificación_de_Texturas/EJERCICIO1_SIN BD/EJERCICIO1_SIN BD/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO1_SIN_BD
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Bitmap bpm2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "Imagenes|*.jpg;*.jpeg;*.png;*.bmp";
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                bmp = new Bitmap(abrir.FileName);
+                pictureOriginal.Image = bmp;
+            }
+        }
+
+    
+
+        private void btnClasificar_Click(object sender, EventArgs e)
+        {
+            if (bmp == null)
+            {
+                MessageBox.Show("Primero cargue una imagen.");
+                return;
+            }
+
+            bpm2 = new Bitmap(bmp.Width, bmp.Height);
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+
+                    int r = pixel.R;
+                    int g = pixel.G;
+                    int b = pixel.B;
+
+                    int promedio = (r + g + b) / 3;
+
+                    Color nuevoColor;
+
+                    if (g > r + 20 && g > b + 20)
+                    {
+                        nuevoColor = Color.FromArgb(34, 197, 94); // Cesped
+                    }
+                    else if (b > r + 25 && b > g + 15)
+                    {
+                        nuevoColor = Color.FromArgb(37, 99, 235); // Agua
+                    }
+                    else if (r > 90 && g > 45 && g < 160 && b < 120 && r > b)
+                    {
+                        nuevoColor = Color.FromArgb(146, 64, 14); // Tierra
+                    }
+                    else if (promedio < 90 && Math.Abs(r - g) < 30 && Math.Abs(g - b) < 30)
+                    {
+                        nuevoColor = Color.FromArgb(17, 24, 39); // Asfalto
+                    }
+                    else if (promedio >= 90 && Math.Abs(r - g) < 35 && Math.Abs(g - b) < 35)
+                    {
+                        nuevoColor = Color.FromArgb(156, 163, 175); // Cemento
+                    }
+                    else
+                    {
+                        nuevoColor = pixel;
+                    }
+
+
+                    bpm2.SetPixel(x, y, nuevoColor);
+                }
+            }
+
+            pictureResultado.Image = bpm2;
+        }
+
+       
+    }
+}
